dcc92325a77e498335996f21df2fd3cbf7d7c102
zhangwentao <zhangwentao@qtone.cn>
2017-08-18 10:02:07 +0800

    注释

--- js/liData.js
+++ js/liData.js
@@ -63,70 +63,69 @@ define({//dedine闭包
 }
 ],
 carLiData:[
-{
-	carName:"雪佛兰 科迈罗",
-	carColor:"黑色",
-	carPlate:"粤T56789",
-	carSeat:"5",
-	carBearing:"500kg",
-	carImg:"images/kwl.png",
-	carState:{name:["可用","车辆状态"],color:"ba_gr",isend:true},
-	Button:{show:true,name:"申请用车",href:"index_usecar.html",id:""}
-},
-{
-	carName:"马自达 昂克赛拉",
-	carColor:"红色",
-	carPlate:"粤T56789",
-	carSeat:"5",
-	carBearing:"500kg",
-	carImg:"images/aksl.png",
-	carState:{name:["出车中","车辆状态"],color:"ba_ye",isend:true},
-	Button:{show:false,name:"申请用车",href:"index_usecar.html",id:""}
-},
-{
-	carName:"东风 雪铁龙",
-	carColor:"蓝色",
-	carPlate:"粤T56789",
-	carSeat:"5",
-	carBearing:"500kg",
-	carImg:"images/xtl.png",
-	carState:{name:["维修中","车辆状态"],color:"ba_red",isend:true},
-	Button:{show:false,name:"申请用车",href:"index_usecar.html",id:""}
-},
-{
-	carName:"东风 雪铁龙",
-	carColor:"蓝色",
-	carPlate:"粤T56789",
-	carSeat:"5",
-	carBearing:"500kg",
-	carImg:"",
-	carState:{name:["维修中","车辆状态"],color:"ba_red",isend:true},
-	Button:{show:false,name:"申请用车",href:"index_usecar.html",id:""}
-}
+	{
+		carName:"雪佛兰 科迈罗",
+		carColor:"黑色",
+		carPlate:"粤T56789",
+		carSeat:"5",
+		carBearing:"500kg",
+		carImg:"images/kwl.png",
+		carState:{name:["可用","车辆状态"],color:"ba_gr",isend:true},
+		Button:{show:true,name:"申请用车",href:"index_usecar.html",id:""}
+	},
+	{
+		carName:"马自达 昂克赛拉",
+		carColor:"红色",
+		carPlate:"粤T56789",
+		carSeat:"5",
+		carBearing:"500kg",
+		carImg:"images/aksl.png",
+		carState:{name:["出车中","车辆状态"],color:"ba_ye",isend:true},
+		Button:{show:false,name:"申请用车",href:"index_usecar.html",id:""}
+	},
+	{
+		carName:"东风 雪铁龙",
+		carColor:"蓝色",
+		carPlate:"粤T56789",
+		carSeat:"5",
+		carBearing:"500kg",
+		carImg:"images/xtl.png",
+		carState:{name:["维修中","车辆状态"],color:"ba_red",isend:true},
+		Button:{show:false,name:"申请用车",href:"index_usecar.html",id:""}
+	},
+	{
+		carName:"东风 雪铁龙",
+		carColor:"蓝色",
+		carPlate:"粤T56789",
+		carSeat:"5",
+		carBearing:"500kg",
+		carImg:"",
+		carState:{name:["维修中","车辆状态"],color:"ba_red",isend:true},
+		Button:{show:false,name:"申请用车",href:"index_usecar.html",id:""}
+	}
 ],
 manLiData:[
-{
-	manName:"王师傅",
-	manIsOut:"粤T0001",
-	manType:"C1",
-	manTx:"images/timg.jpg",
-	manState:{name:["出车中","人员状态"],color:"ba_ye",isend:false},
-},
-{
-	manName:"刘师傅",
-	manIsOut:"待出发",
-	manType:"C1",
-	manTx:"",
-	manState:{name:["空闲中","人员状态"],color:"ba_gr",isend:false},
-},
-{
-	manName:"梁师傅",
-	manIsOut:"待出发",
-	manType:"C1",
-	manTx:"",
-	manState:{name:["空闲中","人员状态"],color:"ba_gr",isend:false},
-}
-
+	{
+		manName:"王师傅",
+		manIsOut:"粤T0001",
+		manType:"C1",
+		manTx:"images/timg.jpg",
+		manState:{name:["出车中","人员状态"],color:"ba_ye",isend:false},
+	},
+	{
+		manName:"刘师傅",
+		manIsOut:"待出发",
+		manType:"C1",
+		manTx:"",
+		manState:{name:["空闲中","人员状态"],color:"ba_gr",isend:false},
+	},
+	{
+		manName:"梁师傅",
+		manIsOut:"待出发",
+		manType:"C1",
+		manTx:"",
+		manState:{name:["空闲中","人员状态"],color:"ba_gr",isend:false},
+	}
 ],
 
 caPlate:{
